137d31199e5a75e0bd1866c2dd35bed92e5f74dc
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-09-17 23:56:29 +0200

    logo code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,12 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 import Logo from './Logo';
-
-import LogoImg from '@/public/logo.svg';
 
 export default function Footer() {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
-          {/* <Image
-            src={LogoImg}
-            alt="CarHub by Jovic Miroslav Frontend Developer"
-            width={118}
-            height={18}
-            className="object-contain"
-          /> */}
           <Logo />
         </div>
       </div>
