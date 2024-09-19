663d311e8b9aa51b845389ccb3269e07b244b6fc
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-09-19 18:30:00 +0200

    search manufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,85 @@
-import { SearchManufacturerProps } from '@/types'
+'use client';
 
-import { Combobox, Transition } from '@headlessui/react'
+import { SearchManufacturerProps } from '@/types';
 
-export default function SearchManufacturer({ manufacturer, setManufacturer }: SearchManufacturerProps) {
-    return (
-        <div>SearchManufacturer</div>
-    )
+import { Combobox, Transition } from '@headlessui/react';
+import Image from 'next/image';
+import { useState, Fragment } from 'react';
+
+import CarLogo from '@/public/car-logo.svg';
+import { manufacturers } from '@/constants';
+
+export default function SearchManufacturer({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) {
+  const [query, setQuery] = useState('');
+
+  const filteredManufacturers =
+    query === ''
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src={CarLogo}
+              alt="Car logo by Jovic Miroslav Frontend Developer"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition duration-100 ease-in"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== '' ? (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  You can build a '{query}' factory if you are a good investor
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    value={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                      }`
+                    }
+                  >
+                    {item}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 }
