471c88d02f74936039dd5cb7f888d3cc2428eb6f
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-09-21 13:01:09 +0200

    data of car

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { CarProps } from '@/types';
+import { calculateCarRent } from '@/utils';
+import Image from 'next/image';
 
 import { CustomButton } from './';
+
+import steeringWheel from '@/public/steering-wheel.svg';
 
 interface CarCardProps {
   car: CarProps;
@@ -11,12 +15,43 @@ interface CarCardProps {
 export default function CarCard({ car }: CarCardProps) {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <article className="car-card group">
       <div className="car-card__content">
         <h2 className="car-card__content-title">
           {make} {model}
         </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src={'/hero.png'}
+          fill
+          priority
+          className="object-contain"
+          alt="car model"
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src={steeringWheel}
+              alt="steeringWheel"
+              width={20}
+              height={20}
+            />
+          </div>
+        </div>
       </div>
     </article>
   );
