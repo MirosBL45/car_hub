a5f800632be6dd5e90f1ad5b7314262cae757a10
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-09-22 23:27:35 +0200

    car image url

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
   const headers = {
-    'x-rapidapi-key': '6d40161bb8msh48091d584932143p17d82djsnb21dd108e904',
+    'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
@@ -29,4 +31,22 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    'customer',
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ''
+  );
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
