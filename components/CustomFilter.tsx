0f4b6c68cd8e5beab58b8634d0221ec7b9239fe6
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-10-22 18:46:36 +0200

    fulters working

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,29 @@
-import { CustomFilterProps } from '@/types'
-import React from 'react'
+'use client';
+
+import { Fragment, useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
 
 export default function CustomFilter({ title, options }: CustomFilterProps) {
-    return (
-        <div>CustomFilter</div>
-    )
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+        }}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
+  );
 }
